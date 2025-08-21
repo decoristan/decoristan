b250d624df8538f266b84a20c27dbe8a2258a69e
decoristan <shaymureed3838@gmail.com>
2025-08-21 14:55:17 +0500

    indexscript.js

---
+++ indexscript.js
@@ -0,0 +1,6 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/sw.js")
+    .then(() => console.log("✅ Service Worker Registered"))
+    .catch((err) => console.error("❌ SW registration failed:", err));
+}
